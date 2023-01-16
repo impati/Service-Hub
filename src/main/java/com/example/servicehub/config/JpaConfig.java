0a25dc09e9ca92b6626dcbb6ce4326c174f8afeb
impati <impati0716@icloud.com>
2023-01-16 21:16:48 +0900

    feat : #13 - jpqQueryFactory 빈 등록

--- src/main/java/com/example/servicehub/config/JpaConfig.java
+++ src/main/java/com/example/servicehub/config/JpaConfig.java
@@ -1,5 +1,6 @@
 package com.example.servicehub.config;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.persistence.EntityManager;
 import java.util.Optional;
 
 @Configuration
@@ -15,9 +17,16 @@ import java.util.Optional;
 @EnableJpaAuditing
 public class JpaConfig {
 
+    private final EntityManager em;
+
     @Bean
     public AuditorAware<String> auditorAware(){
         return () -> Optional.of("impati"); // TODO : 스프링 시큐리티 인증 기능 붙일 때 수정
+    }
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(){
+        return new JPAQueryFactory(em);
     }
 
 }
