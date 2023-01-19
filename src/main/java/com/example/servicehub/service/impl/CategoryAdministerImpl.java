9f6a043a39245a7ea89020e8462e254c5bbde630
impati <impati0716@icloud.com>
2023-01-19 21:12:28 +0900

    feat : #22 - 카테고리 전부 가져오기

---
+++ src/main/java/com/example/servicehub/service/impl/CategoryAdministerImpl.java
@@ -0,0 +1,29 @@
+package com.example.servicehub.service.impl;
+
+import com.example.servicehub.repository.CategoryRepository;
+import com.example.servicehub.service.CategoryAdminister;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class CategoryAdministerImpl implements CategoryAdminister {
+
+    private final CategoryRepository categoryRepository;
+
+    @Override
+    public List<String> getAllCategories() {
+        return categoryRepository.findAll()
+                .stream()
+                .map(category -> category.getName())
+                .collect(Collectors.toList());
+    }
+
+}
