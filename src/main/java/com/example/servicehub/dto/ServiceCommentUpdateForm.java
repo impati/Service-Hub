cfd28a23429536245dd04f24e20cfc8db7264b0c
impati <impati0716@icloud.com>
2023-01-26 20:39:43 +0900

    feat : #34 로그인 사용자 처리

--- src/main/java/com/example/servicehub/dto/ServiceCommentUpdateForm.java
+++ src/main/java/com/example/servicehub/dto/ServiceCommentUpdateForm.java
@@ -2,20 +2,26 @@ package com.example.servicehub.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Data
 @ToString
 @AllArgsConstructor
+@NoArgsConstructor
 public class ServiceCommentUpdateForm {
-    private Long serviceCommentsId;
+    private Long commentId;
     private Long clientId;
     private String content;
     private Long serviceId;
 
-    public ServiceCommentUpdateForm(Long serviceCommentsId, Long clientId, String content) {
-        this.serviceCommentsId = serviceCommentsId;
+    public ServiceCommentUpdateForm(Long commentId, Long clientId, String content) {
+        this.commentId = commentId;
         this.clientId = clientId;
         this.content = content;
+    }
+
+    public void assignClient(Long clientId){
+        this.clientId = clientId;
     }
 }
