0042c421b188e7302b787f4788b6903f9b5a40b3
impati <impati0716@icloud.com>
2023-02-02 20:52:05 +0900

    feat : #46 - provider 유저 정의

---
+++ src/main/java/com/example/servicehub/security/login/GoogleUser.java
@@ -0,0 +1,41 @@
+package com.example.servicehub.security.login;
+
+import com.example.servicehub.domain.Client;
+import com.example.servicehub.domain.ProviderType;
+
+import java.util.Map;
+
+public class GoogleUser extends ProviderUser{
+
+    protected GoogleUser(Map<String, Object> attributes, ProviderType providerType) {
+        super(attributes, providerType);
+    }
+
+    @Override
+    public String getId() {
+        return getAttributeByName("sub");
+    }
+
+    @Override
+    public String getName() {
+        return getAttributeByName("name");
+    }
+
+    @Override
+    public String getEmail() {
+        return getAttributeByName("email");
+    }
+
+    @Override
+    public Client toClient() {
+        return Client.of(
+                getId(),
+                getEmail(),
+                getName(),
+                getEmail(),
+                "ROLE_USER",
+                providerType
+        );
+    }
+
+}
