3fc66af9fc1b54bc7104291d3c8dd86edcf7fe5d
impati <impati0716@icloud.com>
2023-02-05 19:02:47 +0900

    feat : #48 -  서비스 검색 페이지 페이징 및 서비스 추가 및 삭제시 파라미터 유지

--- src/main/java/com/example/servicehub/service/ServiceSearch.java
+++ src/main/java/com/example/servicehub/service/ServiceSearch.java
@@ -5,11 +5,12 @@ import com.example.servicehub.dto.PopularityServiceDto;
 import com.example.servicehub.dto.ServiceSearchConditionForm;
 import com.example.servicehub.dto.SingleServiceWithCommentsDto;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
 
 
 public interface ServiceSearch {
-    Page<PopularityServiceDto> search(ServiceSearchConditionForm serviceSearchConditionForm,Optional<Long> optionalClientId);
+    Page<PopularityServiceDto> search(ServiceSearchConditionForm serviceSearchConditionForm, Optional<Long> optionalClientId, Pageable pageable);
     SingleServiceWithCommentsDto searchSingleService(Long serviceId , Optional<Long> optionalClientId);
 }
