c3bad84ea6a5a2e766d0957e7325fcbaa0607780
impati <impati0716@icloud.com>
2023-02-10 17:49:33 +0900

    feat : #52 - keycloak 회원 가입 validator정의

---
+++ src/main/java/com/example/servicehub/web/validator/annotation/KeycloakUnique.java
@@ -0,0 +1,21 @@
+package com.example.servicehub.web.validator.annotation;
+
+import com.example.servicehub.web.validator.FileSizeValidator;
+import com.example.servicehub.web.validator.KeycloakUsernameUniqueValidator;
+import com.example.servicehub.web.validator.UniqueType;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Target({ElementType.FIELD,ElementType.METHOD
+        ,ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = KeycloakUsernameUniqueValidator.class)
+@Documented
+public @interface KeycloakUnique {
+    Class<? extends Payload> [] payload() default{};
+    Class<?>[] groups() default {};
+    UniqueType uniqueType() default UniqueType.USERNAME;
+    String message() default "이미 등록된 아이디입니다.";
+}
