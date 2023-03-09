8a7964575724233febb67990b7c492b793681a3e
impati <impati0716@icloud.com>
2023-03-09 20:37:53 +0900

    contextLoads 테스트 설정

--- src/test/java/com/example/servicehub/ServiceHubApplicationTests.java
+++ src/test/java/com/example/servicehub/ServiceHubApplicationTests.java
@@ -2,7 +2,10 @@ package com.example.servicehub;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.event.TransactionalEventListener;
 
+@ActiveProfiles("local")
 @SpringBootTest
 class ServiceHubApplicationTests {
 
