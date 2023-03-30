93b928c8642ee7d7ca756ba09a9db3650cb042f1
impati <impati0716@icloud.com>
2023-03-30 20:12:38 +0900

    fix : 특정 서비스 메타 정보 요청시 오류 발생 대응(#92)

--- src/main/java/com/example/servicehub/support/JsoupMetaDataCrawler.java
+++ src/main/java/com/example/servicehub/support/JsoupMetaDataCrawler.java
@@ -1,35 +1,41 @@
 package com.example.servicehub.support;
 
-import com.example.servicehub.exception.ServiceConnectFailException;
+import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
 
-public class JsoupMetaDataCrawler implements MetaDataCrawler{
+@Slf4j
+public class JsoupMetaDataCrawler implements MetaDataCrawler {
 
-    public ServiceMetaData tryToGetMetaData(String serviceUrl){
-        if(serviceUrl == null) return new ServiceMetaData();
-        try{
+    public ServiceMetaData tryToGetMetaData(String serviceUrl) {
+        if (serviceUrl == null) return new ServiceMetaData();
+        try {
             return getMetaData(serviceUrl);
-        }catch (Exception e){
-            throw new ServiceConnectFailException(e.getMessage() + serviceUrl + " 연결에 실패하였습니다");
+        } catch (Exception e) {
+            log.error(e.getMessage() + serviceUrl + " 연결에 실패하였습니다");
+            return new ServiceMetaData();
         }
     }
 
     private ServiceMetaData getMetaData(String serviceUrl) throws IOException {
         Elements elements = Jsoup
                 .connect(serviceUrl)
-                .userAgent("Mozilla/5.0")
+                .timeout(3000)
+                .ignoreContentType(true)
+                .ignoreHttpErrors(true)
+                .followRedirects(false)
+                .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
                 .get()
                 .getElementsByTag("meta");
         ServiceMetaData serviceMetaData = new ServiceMetaData(serviceUrl);
         for (var element : elements) {
-            serviceMetaData.setAttributeByProperty(element.attr("property"),element.attr("content"));
-            serviceMetaData.setAttributeByName(element.attr("name"),element.attr("content"));
+            serviceMetaData.setAttributeByProperty(element.attr("property"), element.attr("content"));
+            serviceMetaData.setAttributeByName(element.attr("name"), element.attr("content"));
         }
 
-        return serviceMetaData ;
+        return serviceMetaData;
     }
 
 }
--- src/test/java/com/example/servicehub/support/JsoupMetaDataCrawlerTest.java
+++ src/test/java/com/example/servicehub/support/JsoupMetaDataCrawlerTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -30,8 +32,9 @@ class JsoupMetaDataCrawlerTest {
     public void serviceConnectTest() throws Exception {
 
         assertThatCode(() -> Jsoup
-                .connect("https://www.udemy.com/")
-                .userAgent("Mozilla/5.0")
+                .connect("https://www.udemy.com")
+                .timeout(5000)
+                .userAgent("Opera")
                 .get()
         ).doesNotThrowAnyException();
     }
@@ -51,7 +54,19 @@ class JsoupMetaDataCrawlerTest {
 
         assertThat(serviceMetaData.getSiteName())
                 .isEqualTo(siteName);
+    }
+
+    @Test
+    @DisplayName("특정 서비스 메타 정보 요청시 오류 발생 테스트")
+    public void WhenTryToGetServiceMetaData() throws Exception {
+
+        List<String> serviceUrlContainer = new ArrayList<>(List.of("https://programmers.co.kr/", "https://www.coupang.com/"));
+
+        for (var serviceUrl : serviceUrlContainer) {
+            assertThatCode(() -> crawler.tryToGetMetaData(serviceUrl)).doesNotThrowAnyException();
+        }
 
     }
+
 
 }
