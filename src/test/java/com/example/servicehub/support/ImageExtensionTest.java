8afb59143c86a8a3c3eb39ec48efcda9232f02f6
impati <impati0716@icloud.com>
2023-04-06 20:14:42 +0900

    test : 이미지 확장자 테스트 수정(#106)

--- src/test/java/com/example/servicehub/support/ImageExtensionTest.java
+++ src/test/java/com/example/servicehub/support/ImageExtensionTest.java
@@ -9,44 +9,39 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class ImageExtensionTest {
+
+    private static Stream<Arguments> extractExtension() {
+        return Stream.of(
+                Arguments.of("file.png", ".png"),
+                Arguments.of("file.PNG", ".png"),
+                Arguments.of("file.dib", ".dib"),
+                Arguments.of("file.png.jpeg", ".jpeg"),
+                Arguments.of("file.raw", ".raw"),
+                Arguments.of("file.tiff", ".tiff"),
+                Arguments.of("file.tif", ".tif"),
+                Arguments.of("file.gif", ".gif"),
+                Arguments.of("file.jpg", ".jpg")
+        );
+    }
 
     @ParameterizedTest
     @MethodSource("extractExtension")
     @DisplayName("이미지 파일에서 확장자 분리")
-    public void extractExtensionTest(String file,String extension) throws Exception{
+    public void extractExtensionTest(String file, String extension) throws Exception {
 
         Assertions.assertThat(ImageExtension.extractExtension(file))
                 .isEqualTo(extension);
 
     }
 
-    private static Stream<Arguments>  extractExtension(){
-        return Stream.of(
-                Arguments.of("file.png",".png"),
-                Arguments.of("file.PNG",".png"),
-                Arguments.of("file.dib",".dib"),
-                Arguments.of("file.png.jpeg",".jpeg"),
-                Arguments.of("file.raw",".raw"),
-                Arguments.of("file.tiff",".tiff"),
-                Arguments.of("file.tif",".tif"),
-                Arguments.of("file.gif",".gif"),
-                Arguments.of("file.jpg",".jpg")
-        );
-    }
-
     @Test
     @DisplayName("지원하는 파일 확장자가 없는 경우")
-    public void notFoundExtension() throws Exception{
-        Assertions.assertThatCode(()->ImageExtension.extractExtension("file.aaa"))
-                .isInstanceOf(IllegalStateException.class);
-
+    public void notFoundExtension() throws Exception {
+        assertThat(ImageExtension.extractExtension("file.aaa")).isEqualTo(".png");
     }
-
-
-
-
-
 
 
 }
