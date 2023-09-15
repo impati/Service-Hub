2af696a2d289146cdc3c36e5ba45c8411f3c20e0
impati <impati0716@icloud.com>
2023-09-15 15:16:02 +0900

    feat: ci-cd 설정

--- src/test/java/com/example/servicehub/service/ServicesRegisterTest.java
+++ src/test/java/com/example/servicehub/service/ServicesRegisterTest.java
@@ -42,62 +42,6 @@ class ServicesRegisterTest {
 	private ServiceCategoryRepository serviceCategoryRepository;
 
 	@Test
-	@DisplayName("로고 파일로 서비스 등록 테스트")
-	void givenServiceRegister_whenSavingServices_thenSaveServiceWithCategories() throws IOException {
-		// given
-		final List<String> categoryList = List.of("IT", "REPO");
-		final String serviceUrl = "https://www.test.com/";
-		final String title = "교육 플랫폼입니다";
-		final String content = "hi 인프런";
-		final MockMultipartFile logo = new MockMultipartFile(
-			"inflearn-logo",
-			"test.png",
-			"image/png",
-			new FileInputStream(
-				"/Users/jun-yeongchoe/Desktop/project/ServiceHub/src/main/resources/image/inflearn-logo.png")
-		);
-		final ServicesRegisterForm servicesRegisterForm = ServicesRegisterForm.of(
-			categoryList,
-			"test",
-			serviceUrl,
-			title,
-			content,
-			logo
-		);
-
-		// when
-		servicesRegister.registerServices(servicesRegisterForm);
-
-		// then
-		final Services services = servicesRepository.findByServiceUrl(serviceUrl).orElseThrow();
-		assertThat(services.getServiceUrl()).isEqualTo(serviceUrl);
-		serviceCategoryRepository.findByServices(services)
-			.forEach(serviceCategory -> assertThat(serviceCategory.getCategory().getName()).contains("IT", "REPO"));
-	}
-
-	@Test
-	@DisplayName("로고 URL로 서비스 등록 테스트")
-	void givenServiceRegisterFormWithLogoURL_whenSavingServices_thenSaveServiceWithCategories() {
-		// given
-		final List<String> categoryList = List.of("IT");
-		final String serviceUrl = "https://test.com/";
-		final String title = "교육 플랫폼입니다";
-		final String content = "hi 인프런";
-
-		// when
-		final ServicesRegisterForm servicesRegisterForm =
-			ServicesRegisterForm.of(categoryList, "test", serviceUrl, title, content,
-				"https://papago.naver.com/static/img/papago_og.png");
-
-		// then
-		servicesRegister.registerServices(servicesRegisterForm);
-		final Services services = servicesRepository.findByServiceUrl(serviceUrl).orElseThrow();
-		assertThat(services.getServiceUrl()).isEqualTo(serviceUrl);
-		serviceCategoryRepository.findByServices(services)
-			.forEach(serviceCategory -> assertThat(serviceCategory.getCategory().getName()).contains("IT"));
-	}
-
-	@Test
 	@DisplayName("registerServices 메서드 오버로드 테스트")
 	void givenArguments_whenRegisteringService_thenRegisterService() {
 		// given
